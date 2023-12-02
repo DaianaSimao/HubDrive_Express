3684066fd32d01ad8a600abc2f7ed623d0697bcb
DaianaSimao <daiana.xd11@hotmail.com>
2023-12-02 18:03:45 -0300

    Feat(People) : Criação da tebla people.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_29_220352) do
+ActiveRecord::Schema.define(version: 2023_12_02_205428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,8 +24,6 @@ ActiveRecord::Schema.define(version: 2023_11_29_220352) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "login", default: "", null: false
-    t.string "name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["login"], name: "index_users_on_login", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
