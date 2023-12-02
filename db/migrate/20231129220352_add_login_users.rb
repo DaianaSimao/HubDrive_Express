0c5746e35827df67b6109943569ab71bce5e20b6
DaianaSimao <daiana.xd11@hotmail.com>
2023-12-02 18:04:21 -0300

    FIx(Users): Retiro as colunas name e last_name da tabela Users.

--- db/migrate/20231129220352_add_login_users.rb
+++ db/migrate/20231129220352_add_login_users.rb
@@ -2,7 +2,5 @@ class AddLoginUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :login, :string, null: false, default: ""
     add_index :users, :login, unique: true
-    add_column :users, :name, :string
-    add_column :users, :last_name, :string
   end
 end
