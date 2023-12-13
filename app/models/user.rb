f7f6dbfd46abaa22c4614ae13fe2e3287a542134
DaianaSimao <daiana.xd11@hotmail.com>
2023-12-13 15:18:38 -0300

    Fix(Autenticação): Solução do bug do scope da api do github

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
       existing_user.update!(provider: auth.provider, uid: auth.uid)
       existing_user
     else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      self.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.email = auth.info.email
         user.password = Devise.friendly_token[0, 20]
         user.password_confirmation = user.password
